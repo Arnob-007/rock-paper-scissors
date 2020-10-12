0dd501972fa6bdd5e2803cb92f11f105c91c0028
Arnob_007 <Arnob-007@users.noreply.github.com>
2020-10-12 16:37:54 +0600

    Updates

---
+++ precache-manifest.0186af091fd48c89ba655e0dabe0ba5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4683634211b1e1810d0c467677cf1419",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "bb1cc9d202e27cf0b455",
+    "url": "/rock-paper-scissors/static/css/main.7d0d42b2.chunk.css"
+  },
+  {
+    "revision": "4c8098795803d5ca7585",
+    "url": "/rock-paper-scissors/static/js/2.72096de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.72096de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1cc9d202e27cf0b455",
+    "url": "/rock-paper-scissors/static/js/main.f4a27c53.chunk.js"
+  },
+  {
+    "revision": "694c8e56d562043498f6",
+    "url": "/rock-paper-scissors/static/js/runtime-main.cfd905d5.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/rock-paper-scissors/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/rock-paper-scissors/static/media/image-rules.5d74d25d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.833138b16c3807016414720af666b123.js"
+  "/rock-paper-scissors/precache-manifest.0186af091fd48c89ba655e0dabe0ba5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rock-paper-scissors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
